76c41c96121ab8262194fe7540f0019985f94c0a
xuzewei <xuzewei_2012@126.com>
2018-01-24 22:54:55 +0800

    下单

--- src/main/java/com/github/microprograms/yy_vip_center_site_api/public_api/MixOrder_Buy_Api.java
+++ src/main/java/com/github/microprograms/yy_vip_center_site_api/public_api/MixOrder_Buy_Api.java
@@ -1,5 +1,9 @@
 package com.github.microprograms.yy_vip_center_site_api.public_api;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import com.alibaba.fastjson.JSON;
 import com.github.microprograms.micro_api_runtime.annotation.MicroApi;
 import com.github.microprograms.micro_api_runtime.exception.MicroApiPassthroughException;
@@ -9,12 +13,14 @@ import com.github.microprograms.micro_api_runtime.utils.MicroApiUtils;
 import com.github.microprograms.micro_nested_data_model_runtime.Comment;
 import com.github.microprograms.micro_nested_data_model_runtime.Required;
 import com.github.microprograms.micro_oss_core.MicroOss;
+import com.github.microprograms.micro_oss_core.model.Field;
+import com.github.microprograms.micro_oss_core.model.dml.Condition;
 import com.github.microprograms.yy_vip_center_site_api.utils.Fn;
 
 @MicroApi(comment = "商品订单 - 购买", type = "read", version = "v0.0.6")
 public class MixOrder_Buy_Api {
 
-    private static MixOrder buildMixOrder(Req req) throws Exception {
+    private static void core(Req req, Response resp) throws Exception {
         User user = Fn.queryUserByToken(req.getToken());
         if (user == null) {
             throw new MicroApiPassthroughException(ErrorCodeEnum.invalid_token);
@@ -26,21 +32,44 @@ public class MixOrder_Buy_Api {
         if (goods.getStock() <= 0) {
             throw new MicroApiPassthroughException(ErrorCodeEnum.low_stock);
         }
+        int orderAmount = goods.getPrice();
+        int oldWalletAmount = user.getWalletAmount();
+        int newWalletAmount = oldWalletAmount - orderAmount;
+        if (newWalletAmount < 0) {
+            throw new MicroApiPassthroughException(ErrorCodeEnum.low_wallet_amount);
+        }
+        String mixOrderId = Fn.genNewMixOrderId();
+        // 订单
         MixOrder mixOrder = new MixOrder();
-        mixOrder.setId(Fn.genNewMixOrderId());
+        mixOrder.setId(mixOrderId);
         mixOrder.setUserId(user.getId());
         mixOrder.setUserNickname(user.getNickname());
-        mixOrder.setOrderAmount(goods.getPrice());
+        mixOrder.setOrderAmount(orderAmount);
         mixOrder.setGoodsId(goods.getId());
         mixOrder.setGoodsName(goods.getName());
         mixOrder.setGoodsDetail(JSON.toJSONString(goods));
         mixOrder.setComment(null);
         mixOrder.setDtCreate(System.currentTimeMillis());
-        return mixOrder;
-    }
-
-    private static void core(Req req, Response resp) throws Exception {
-        MicroOss.insertObject(buildMixOrder(req));
+        MicroOss.insertObject(mixOrder);
+        // 用户
+        List<Field> userFields = new ArrayList<>();
+        userFields.add(new Field("walletAmount", newWalletAmount));
+        MicroOss.updateObject(User.class, userFields, Condition.build("id=", user.getId()));
+        // 钱包账单
+        WalletBill walletBill = new WalletBill();
+        walletBill.setId(UUID.randomUUID().toString());
+        walletBill.setUserId(user.getId());
+        walletBill.setUserNickname(user.getNickname());
+        // 类型(1入账,2消费)
+        walletBill.setType(2);
+        walletBill.setDtCreate(System.currentTimeMillis());
+        walletBill.setAmount(orderAmount);
+        walletBill.setOldWalletAmount(oldWalletAmount);
+        walletBill.setNewWalletAmount(newWalletAmount);
+        walletBill.setOutOrderGoodsId(goods.getId());
+        walletBill.setOutOrderGoodsName(goods.getName());
+        walletBill.setOutOrderId(mixOrderId);
+        MicroOss.insertObject(walletBill);
     }
 
     public static Response execute(Request request) throws Exception {
